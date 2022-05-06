5cbd322a3537164d5fb980dafd92da5224234687
Eduardo <luiztebar@gmail.com>
2022-05-06 14:01:49 -0300

    Metodo main da classe fatorial excluido e movido para a classe TesteFatorial.java

--- Fatorial.java
+++ Fatorial.java
@@ -1,10 +1,5 @@
 public class Fatorial{
-	
-	public static void main(String[] args){
-		System.out.println(calculo(5));
-	}
-
-	public static int calculo(int n){
+	public int calculo(int n){
 	int resultado = 1;
 	for (int i=2;i<=n;i++){
 		resultado *= i;
